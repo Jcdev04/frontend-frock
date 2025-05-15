e9a025efffea386e420b6855a697098b6449a76b
Mitawarmi2000 <u202214130@upc.edu.pe>
2025-05-14 21:10:03 -0500

    feat(Appvue): added application codes.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,44 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <RutasView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import RutasView from './combiroutes/presentation/views/RutasView.vue'
+
+export default {
+  name: 'App',
+  components: {
+    RutasView
+  }
+}
+</script>
+
+<style>
+:root {
+  --color-primary: #7A78FF;
+  --color-off: #CCCCFF;
+  --color-secondary: #F8F9F0;
+  --color-tertiary-gray: #E8E0E8;
+  --color-tertiary-inputs: #BCB73E;
+  --color-text: #000000;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+body {
+  font-family: Arial, sans-serif;
+  background-color: white;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  width: 100%;
+  min-height: 100vh;
+  background-color: white;
 }
 </style>

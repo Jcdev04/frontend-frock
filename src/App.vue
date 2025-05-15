df647ed880c317f449d18c713b91acc87c5acebe
Mitawarmi2000 <u202214130@upc.edu.pe>
2025-05-14 21:32:44 -0500

    feat(app): added toolbar application.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,55 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="app">
+    <!-- El toolbar solo se muestra si el usuario está autenticado -->
+    <app-toolbar v-if="isAuthenticated" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <!-- Router view para mostrar el contenido de cada ruta -->
+    <router-view />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import AppToolbar from './components/AppToolbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppToolbar
+  },
+  data() {
+    return {
+      // Por ahora lo dejamos como true para desarrollo
+      // En producción, esto vendría de un store o servicio de autenticación
+      isAuthenticated: true
+    }
+  }
+}
+</script>
+
+<style>
+:root {
+  --color-primary: #7A78FF;
+  --color-off: #CCCCFF;
+  --color-secondary: #F8F9F0;
+  --color-tertiary-gray: #E8E0E8;
+  --color-tertiary-inputs: #BCB73E;
+  --color-text: #000000;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+body {
+  font-family: Arial, sans-serif;
+  background-color: white;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  width: 100%;
+  min-height: 100vh;
+  background-color: white;
 }
 </style>

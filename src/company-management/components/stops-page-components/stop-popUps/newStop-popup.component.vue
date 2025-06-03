fe3e8df75d76e315765c9eca7057a35495fd3302
AmirbarrabajaLeon <amircasan@gmail.com>
2025-06-03 13:58:31 -0500

    components(stops-page-components): created a stop-popUps folder and stop-popUp vue component

---
+++ src/company-management/components/stops-page-components/stop-popUps/newStop-popup.component.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "newStop-popup.component.vue"
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>

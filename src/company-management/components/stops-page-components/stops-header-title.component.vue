763e4e5e781a0b17727d2ea43414ae3d0706c984
AmirbarrabajaLeon <amircasan@gmail.com>
2025-06-03 15:54:53 -0500

    newStop-popup.component.vue: moved the button to the popUpNewStop component

--- src/company-management/components/stops-page-components/stops-header-title.component.vue
+++ src/company-management/components/stops-page-components/stops-header-title.component.vue
@@ -1,6 +1,4 @@
 <script>
-
-import { ref } from "vue"; //principalmente lo usamos para el atributo visible del button
 import popUpNewStop from "../stops-page-components/stop-popUps/newStop-popup.component.vue"; //popUp para cuando se clickee "nuevo paradero"
 
 export default {
@@ -9,22 +7,13 @@ export default {
   components: {
     popUpNewStop
   },
-
-  setup() {
-    const visible = ref(false); //variable visible que controlara la aparicion del popUp
-
-
-    return {visible}
-  },
 }
 </script>
 
 <template>
   <div class="container">
-
     <h1>Paraderos</h1>
-
-    <pb-Button class="nuevo-paradero-button" icon="pi pi-plus" label="Nuevo Paradero"/>
+    <pop-up-new-stop/>
 
   </div>
 </template>
@@ -42,37 +31,4 @@ export default {
   font-weight: bold;
   color: #7A78FF;
 }
-
-.nuevo-paradero-button{
-  /*Flex para centrar*/
-  display: flex;
-  align-items: center;
-
-  /*Button en si*/
-  background-color: #7A78FF;
-
-  border: none;
-  border-radius: 8px;
-
-  padding: 8px 16px;
-
-  max-height: 40px;
-  height: 100%;
-
-  max-width: 200px;
-  width: 100%;
-
-  /*Text Format*/
-  font-size: 16px;
-  font-family: Poppins, sans-serif;
-  color: white;
-}
-
-.nuevo-paradero-button.p-button:hover{
-  cursor: pointer;
-  background-color: #CCF;
-  color: white;
-  border: none;
-}
-
 </style>

f125090e2ba5086919e59e1a7869a2d2e339dc9c
AmirbarrabajaLeon <amircasan@gmail.com>
2025-06-04 01:39:53 -0500

    stop-card.component.vue: now stop card get created based on how many cards are in the db.json

--- src/company-management/components/stops-page-components/stops-list.component.vue
+++ src/company-management/components/stops-page-components/stops-list.component.vue
@@ -1,22 +1,56 @@
 <script>
-/**
- * Should receive props: isCRUD, to enable settings icon
- * */
 import stopCard from "./stop-card/stop-card.component.vue";
+import { StopService } from '../../services/stop-services/stop.service.js';
 
 export default {
   name: "stopsList",
   components: {
-    stopCard,
+    stopCard
+  },
+
+  data() {
+    return {
+      stops: [],
+      isLoading: false,
+      error: null
+    };
+  },
+
+  methods: {
+    async loadStops() {
+      this.isLoading = true;
+      this.error = null;
+      try {
+        const service = new StopService();
+        this.stops = await service.getStops();
+      } catch (err) {
+        this.error = `Error al cargar paraderos: ${err.message}`;
+        // Mostrar toast
+        this.$toast.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: this.error
+        });
+      } finally {
+        this.isLoading = false;
+      }
+    }
+  },
+  mounted() {
+    this.loadStops();
   }
 }
 
 </script>
 
 <template>
-  <section class="container-paraderos">
-    <stop-card/>
-    <stop-card/>
+  <pb-Toast/>
+  <pb-Progress-spinner v-if="isLoading" />
+  <pb-Message v-if="error" severity="error" :closable="false">
+    {{ error }}
+  </pb-Message>
+  <section v-for="stop in stops" :key="stop.id" class="container-paraderos">
+    <stop-card :stop="stop" />
   </section>
 </template>
 

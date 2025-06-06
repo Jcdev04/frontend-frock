5f43af79e5c2997b0541c661e1c69c2a9676604d
AmirbarrabajaLeon <amircasan@gmail.com>
2025-06-06 18:06:45 -0500

    stops-popUps: created a delete Stop Pop UP component

---
+++ src/company-management/components/stops-page-components/stop-popUps/deleteStop-popup.component.vue
@@ -0,0 +1,72 @@
+<script>
+import { useConfirm } from 'primevue/useconfirm';
+import { StopService } from '../../../services/stop-services/stop.service.js';
+import { useToast } from 'primevue/usetoast';
+
+export default {
+  name: 'deleteStopPopUp',
+
+  props: {
+    stopId: {
+      type: String,
+      required: true
+    }
+  },
+
+  setup(props, {emit}) {
+    const confirm = useConfirm();
+    const toast = useToast();
+    const service = new StopService();
+
+    const confirmDelete = () => {
+      confirm.require({
+        message: '¿Estás seguro de que quieres eliminar este paradero?',
+        header: 'Confirmar Eliminación',
+        icon: 'pi pi-exclamation-triangle',
+        acceptLabel: 'Sí',
+        rejectLabel: 'No',
+        accept: async () => {
+          try {
+            await service.deleteStop(props.stopId);
+            toast.add({
+              severity: 'success',
+              summary: 'Éxito',
+              detail: 'Paradero eliminado',
+              life: 3000
+            });
+            emit('deleted', props.stopId);
+          } catch (error) {
+            toast.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: `Error al eliminar paradero: ${error.message}`,
+              life: 3000
+            });
+          }
+        },
+        reject: () => {
+          // No hacer nada
+        }
+      });
+    };
+    return { confirmDelete };
+  }
+}
+
+</script>
+
+<template>
+  <pb-Button icon="pi pi-trash"  class="delete-button" @click="confirmDelete" />
+</template>
+
+<style scoped>
+.delete-button{
+  background-color: #FF2C2C;
+  border-color: transparent;
+}
+
+.delete-button.p-button:hover {
+  background-color: lightcoral;
+  border-color: transparent;
+}
+</style>

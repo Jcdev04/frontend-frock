689fa4004c3c3176efa7b005afc73be990f3abc5
AmirbarrabajaLeon <amircasan@gmail.com>
2025-06-10 18:19:12 -0500

    stop-page-components(stops-list): moved loadstops methods and other to stopspage which would be it's parent, also emits based on the emits it receives from deletestop and editstop

--- src/company-management/components/stops-page-components/stops-list.component.vue
+++ src/company-management/components/stops-page-components/stops-list.component.vue
@@ -1,6 +1,6 @@
 <script>
 import stopCard from "./stop-card/stop-card.component.vue";
-import { StopService } from '../../services/stop-services/stop.service.js';
+
 
 export default {
   name: "stopsList",
@@ -8,38 +8,22 @@ export default {
     stopCard
   },
 
-  data() {
-    return {
-      stops: [],
-      isLoading: false,
-      error: null
-    };
-  },
-
-  methods: {
-
-    async loadStops() {
-      this.isLoading = true;
-      this.error = null;
-      try {
-        const service = new StopService();
-        this.stops = await service.getStops();
-      } catch (err) {
-        this.error = `Error al cargar paraderos: ${err.message}`;
-        // Mostrar toast
-        this.$toast.add({
-          severity: 'error',
-          summary: 'Error',
-          detail: this.error
-        });
-      } finally {
-        this.isLoading = false;
-      }
+  props:{
+    stops: {
+      type: Array,
+      required: true
+    },
+    isLoading: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String,
+      default: null
     }
   },
-  mounted() {
-    this.loadStops();
-  }
+
+  emits: ['updated', 'deleted']
 }
 
 </script>
@@ -50,9 +34,17 @@ export default {
   <pb-Message v-if="error" severity="error" :closable="false">
     {{ error }}
   </pb-Message>
-  <section v-for="stop in stops" :key="stop.id" class="container-paraderos">
-    <stop-card :stop="stop" />
+
+  <section class="container-paraderos">
+    <stop-card
+        v-for="stop in stops"
+        :key="stop.id"
+        :stop="stop"
+        @updated="$emit('updated', $event)"
+        @deleted="$emit('deleted', $event)"
+    />
   </section>
+
   <pb-ConfirmDialog/>
 
 </template>
@@ -70,6 +62,6 @@ export default {
   padding: 10px 0;
 
   /**/
-  gap: 10px;
+  gap: 20px;
 }
 </style>
